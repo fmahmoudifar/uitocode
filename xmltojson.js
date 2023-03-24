94c38eb56982b3a25e7aa588bbd5cd383ed2c8a3
Farshad Mahmoudifar <fmahmoudifar@gmail.com>
2023-03-24 21:31:52 +0200

    add, modify parser, install dependecies

--- xmltojson.js
+++ xmltojson.js
@@ -1,3 +1,32 @@
 const fs = require('fs');
+const xml2js = require('xml2js');
+
+const xml = fs.readFileSync('ui.xml', 'utf8');
+
+const parser = new xml2js.Parser();
+let json = {};
+
+parser.parseString(xml, function (err, result) {
+    if (err) throw err;
+    json = result;
+});
+
+const getStyle = component => {
+    const { $ } = component;
+    if ($ && $.style) {
+        $.style = $.style.split(';').reduce((acc, curr) => {
+            const [key, value] = curr.split('=');
+            acc[key] = value;
+            return acc;
+        }, {});
+    }
+    component.mxCell?.forEach(cell => getStyle(cell));
+};
+
+json.mxfile.diagram.forEach(diagram => getStyle(diagram.mxGraphModel[0].root[0]));
 
 
+fs.writeFile(__dirname + '/ui.json', JSON.stringify(json, null, 2), function (err) {
+    if (err) throw err;
+    console.log('Conversion has been completed');
+});
