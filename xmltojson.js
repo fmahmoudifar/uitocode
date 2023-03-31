459d3674a40702d8c894eba55e3b9ab8a9e326d5
Farshad Mahmoudifar <fmahmoudifar@gmail.com>
2023-04-01 00:29:01 +0300

    modifying code for writing

--- xmltojson.js
+++ xmltojson.js
@@ -23,40 +23,40 @@ const fs = require('fs');
 const xml2js = require('xml2js');
 
 
-// let json = {};
-// function parser() {
-const xml = fs.readFileSync('ui.xml', 'utf8');
-
-const parser = new xml2js.Parser();
 let json = {};
+function parser() {
+    const xml = fs.readFileSync('ui.xml', 'utf8');
 
-parser.parseString(xml, function (err, result) {
-    if (err) throw err;
-    json = result;
-});
+    const parser = new xml2js.Parser();
+    // let json = {};
 
-const getStyle = component => {
-    const { $ } = component;
-    if ($ && $.style) {
-        $.style = $.style.split(';').reduce((acc, curr) => {
-            const [key, value] = curr.split('=');
-            acc[key] = value;
-            return acc;
-        }, {});
-    }
-    component.mxCell?.forEach(cell => getStyle(cell));
+    parser.parseString(xml, function (err, result) {
+        if (err) throw err;
+        json = result;
+    });
+
+    const getStyle = component => {
+        const { $ } = component;
+        if ($ && $.style) {
+            $.style = $.style.split(';').reduce((acc, curr) => {
+                const [key, value] = curr.split('=');
+                acc[key] = value;
+                return acc;
+            }, {});
+        }
+        component.mxCell?.forEach(cell => getStyle(cell));
+    };
+
+    json.mxfile.diagram.forEach(diagram => getStyle(diagram.mxGraphModel[0].root[0]));
+
+    return json;
+}
+// fs.writeFile(__dirname + '/ui.json', JSON.stringify(json, null, 2), function (err) {
+//     if (err) throw err;
+//     console.log('Conversion has been completed');
+// });
+// console.log(json);
+
+module.exports = {
+    parser: parser
 };
-
-json.mxfile.diagram.forEach(diagram => getStyle(diagram.mxGraphModel[0].root[0]));
-
-// return json;
-// }
-fs.writeFile(__dirname + '/ui.json', JSON.stringify(json, null, 2), function (err) {
-    if (err) throw err;
-    console.log('Conversion has been completed');
-});
-console.log(json);
-
-// module.exports = {
-//     parser: parser
-// };
