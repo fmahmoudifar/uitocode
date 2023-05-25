63f5eaa0791c74ed75f1396dd8ea6eb789168e7b
Farshad Mahmoudifar <fmahmoudifar@gmail.com>
2023-05-25 12:16:42 +0300

    updated the HTML code

--- uitocode.js
+++ uitocode.js
@@ -59,7 +59,7 @@ function create() {
             });
 
             function setComponent() {
-                fs.writeFile(htmlComponent, `<!DOCTYPE html>\n<html lang="en">\n<head>\n<meta charset="UTF-8">\n<meta http-equiv="X-UA-Compatible" content="IE=edge">\n<meta name="viewport" content="width=device-width, initial-scale=1.0">\n<title>${name}</title>\n</head>\n<body>\n`,
+                fs.writeFile(htmlComponent, `<!DOCTYPE html>\n<html lang="en">\n<head>\n<meta charset="UTF-8">\n<title>${name}</title>\n</head>\n<body>\n`,
                     (err) => {
                         if (err) throw err;
                         console.log(`Cleared out ${name}.component.html`);
